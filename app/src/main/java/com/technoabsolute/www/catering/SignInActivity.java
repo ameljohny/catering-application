e300e76bef8fc6fa42ed8f62ec039f550b7e1769
ameljohny <amel.johny@gmail.com>
2017-11-26 19:23:57 +0530

    -color transparent error resolved -actionBar removed from signIn page

--- app/src/main/java/com/technoabsolute/www/catering/SignInActivity.java
+++ app/src/main/java/com/technoabsolute/www/catering/SignInActivity.java
@@ -3,11 +3,13 @@ package com.technoabsolute.www.catering;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signin);
+
+        getSupportActionBar().hide();
     }
 }
